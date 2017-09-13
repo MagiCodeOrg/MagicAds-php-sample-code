408c1d8f3ae987d7785549f0f675bd3a503e72f5
ONYX <onyx.tm.mb@gmail.com>
2017-09-13 22:53:54 +0430

    Add files via upload

---
+++ SDK/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Magicode.
+ * User: Mench
+ */
+
+
+define("MAGICODE_KEY", "XXXXX-XXXXXX-XXXXXX-XXXXX");// (str - int)XXXXX-XXXXXX-XXXXXX-XXXXX  25Char
+define("TELEGRAM_BOT_TOKEN", "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"); // (int)XXXXXXXXX:(str - int)XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 45Char
+define("MAGICODE_API_URL", "http://ads.magicode.ir/api/"); // Magicode API URL
+
+/**
+ * Methodes
+ */
+define("GO_TO_ADS", "goToAds");
+define("GET_ADS", "getAds");
+
